d9e65e84b6b892287969bced3db2c7db7fcf47fb
Ahmet Samet Demiralay <ahmetsametdemiralay@Ahmet-MacBook-Air.local>
2024-02-06 21:43:20 +0300

    updated env2

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_KEY,
+  baseURL: `https://${process.env.REACT_APP_DOG_URL}`,
 });
 
 export default instance;
